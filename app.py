256076c9d205d8ad07fc7ba995f63e066088a15f
catiradot <catirado_usistemas@intep.edu.co>
2026-04-09 18:16:51 -0500

    Actualizacion Notebook para diseño Appy

--- app.py
+++ app.py
@@ -889,25 +889,14 @@ with tab2:
             fontsize=8
         )
         g.fig.subplots_adjust(bottom=0.18)
-        g.fig.patch.set_facecolor('#1b2838')
+        g.fig.patch.set_facecolor('#0e1117')
         for ax in g.axes.flatten():
-            ax.set_facecolor('#243447')
+            ax.set_facecolor('#0e1117')
             ax.tick_params(colors='white')
             ax.xaxis.label.set_color('white')
             ax.yaxis.label.set_color('white')
-            ax.title.set_color('white') if ax.get_title() else None
             for spine in ax.spines.values():
-                spine.set_edgecolor('#4a6fa5')
-                spine.set_linewidth(0.8)
-        g.fig.patch.set_alpha(0.95)
-        for ax in g.axes.flatten():
-            ax.patch.set_alpha(0.85)
-        if g._legend:
-            g._legend.get_frame().set_facecolor('#1b2838')
-            g._legend.get_frame().set_edgecolor('#4a6fa5')
-            for text in g._legend.get_texts():
-                text.set_color('white')
-            g._legend.get_title().set_color('white')
+                spine.set_edgecolor('#444')
         plt.tight_layout()
         st.pyplot(g.fig, use_container_width=True)
         plt.close('all')
